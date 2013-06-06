99def014014829d447ff34b8c57a1f3a8613602f
Beatriz Medina <beatrizmmr@gmail.com>
2013-06-06 00:30:11 -0700

    Implementacion del patron Decorator para la construccion del objeto Mariposa.

---
+++ SrcCode/AlgoritmoGenetico/AlgoritmoGenetico/Mariposa/Component/Mariposa.cs
@@ -0,0 +1,19 @@
+﻿using AlgoritmoGenetico.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmoGenetico
+{
+    public abstract class Mariposa
+    {
+        Ala ala;
+        public string Descripcion { get; set; }
+        public Espiritrompa espiritrompa { get; set; }
+        public Antena antena { get; set; }
+
+        public abstract string ObtieneDescripcion();
+    }
+}
