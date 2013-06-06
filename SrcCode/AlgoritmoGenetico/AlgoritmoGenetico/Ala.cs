374003c0dd840d1996c35657fb79ab85ba90cdf9
Beatriz Medina <beatrizmmr@gmail.com>
2013-06-05 18:03:55 -0700

    Creacion de clase Genotipo y Ala.

---
+++ SrcCode/AlgoritmoGenetico/AlgoritmoGenetico/Ala.cs
@@ -0,0 +1,14 @@
+﻿using AlgoritmoGenetico.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoritmoGenetico
+{
+    public class Ala
+    {
+        Color color;
+        Forma forma;
+    }
+}
---
+++ SrcCode/AlgoritmoGenetico/AlgoritmoGenetico/Genotipo.cs
@@ -0,0 +1,16 @@
+﻿using AlgoritmoGenetico.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmoGenetico
+{
+    public class Genotipo
+    {
+        Espiritrompa espiritrompa;
+        Antena antena;
+        Ala ala;
+    }
+}
